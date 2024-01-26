da8844262cc2076dc30e6ede97bd6dde6b1b9bc8
minhajakhan <minhajiialamkhan@gmail.com>
2024-01-26 18:28:43 -0500

    created database using python shell

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite.db'
+db = SQLAlchemy(app)
+app.app_context().push()
 
+#create models
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean, default=False)
 
 @app.route('/')
 def index():
@@ -12,4 +21,6 @@ def about():
     return render_template('dashboard/about.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True)
