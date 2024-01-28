93e182c231ae6570ad475821dd94aebbaf63c00e
minhajakhan <minhajiialamkhan@gmail.com>
2024-01-28 14:07:20 -0500

    todo app complete with marquee and all

--- app.py
+++ app.py
@@ -15,13 +15,31 @@ class Todo(db.Model):
 @app.route('/')
 def index():
     todo_list = Todo.query.all()
-    return render_template('dashboard/index.html', todo_list=todo_list)
+    total_todo = Todo.query.count()
+    completed_todo = Todo.query.filter_by(complete=True).count()
+    uncompleted_todo = total_todo - completed_todo
+    return render_template('dashboard/index.html', todo_list=todo_list, total_todo=total_todo, completed_todo=completed_todo, uncompleted_todo=uncompleted_todo)
+    #can use **locals as 2nd postional arguemnt instead of being verbose and listing each one
 
 @app.route('/add', methods=['POST'])
 def add():
     title = request.form.get('title')
     new_todo = Todo(title=title, complete=False)
     db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    todo = Todo.query.filter_by(id=id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/update/<int:id>')
+def update(id):
+    todo = Todo.query.filter_by(id=id).first()
+    todo.complete = not todo.complete
     db.session.commit()
     return redirect(url_for('index'))
 
